400dc3496f4acf80772e18118b90289bd8047ac2
Roman <romuland@mail.ru>
2012-11-09 00:18:48 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+First::Application.config.secret_token = '330c844e873eab65710f0cfdeb7da8bc81f6123cdfdcd33364879e83467eda6f62363b78e1276667e82269c85a2ec4bc2c252fd92d3e428acc8637303042679e'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+First::Application.routes.draw do
+  get "welcome/index"
+
+root :to => 'welcome#index'
+end
